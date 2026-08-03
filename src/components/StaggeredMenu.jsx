a682e66402f81d13c6986d1103f3f3db97c8251c
ashu ulape <ashuulape@gmail.com>
2026-08-04 00:01:48 +0530

    added link

--- src/components/StaggeredMenu.jsx
+++ src/components/StaggeredMenu.jsx
@@ -12,8 +12,8 @@ const item = [
 
 const socialItem = [
   { label: "GitHub", link: "https://github.com/ashuulape" },
-  { label: "LinkedIn", link: "" },
-  { label: "Instagram", link: "https://twitter.com" },
+  { label: "LinkedIn", link: "www.linkedin.com/in/ashutosh-ulape-6b43a4325" },
+  { label: "Instagram", link: "" },
 ];
 
 export const StaggeredMenu = ({
